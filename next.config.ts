f859048eef6d1701f6ff6ea0467a5cf8a0bb3c51
Mumukshu D.C <phoenixrising656@gmail.com>
2026-03-01 21:14:37 +0545

    feat: setup GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	output: "export",
+	basePath: "/nep-counselor-tool",
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
